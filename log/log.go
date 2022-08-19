944ea6cb685e713795cf5624a028cabc1982adc9
muckas <motobike8@outlook.com>
2022-08-19 15:02:34 +0500

    Logging

---
+++ log/log.go
@@ -0,0 +1,58 @@
+package log
+
+import (
+	"fmt"
+	"os"
+	"time"
+	"path/filepath"
+)
+
+var (
+	LOGDIR = ""
+	LOGNAME = ""
+)
+
+func logWrite(line string, level string) {
+	now := time.Now()
+	datetime := now.Format("2006-01-02 15:04:05.000")
+	formatted_line := fmt.Sprintf("%s : %s : %s\n", datetime, level, line)
+	fmt.Printf(formatted_line)
+
+	date := now.Format("2006-01-02")
+	logfile := fmt.Sprintf("%s-%s.log", date, LOGNAME)
+
+	f, err := os.OpenFile(filepath.Join(LOGDIR, logfile), os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer f.Close()
+	_, err = f.WriteString(formatted_line)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func Debug(line string) {
+	logWrite(line, "DEBUG")
+}
+
+func Info(line string) {
+	logWrite(line, "INFO")
+}
+
+func Warning(line string) {
+	logWrite(line, "WARNING")
+}
+
+func Error(line string) {
+	logWrite(line, "ERROR")
+}
+
+func Init(logdir string, logname string) {
+	LOGDIR = logdir
+	LOGNAME = logname
+	err := os.Mkdir(logdir, 0666)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
--- main.go
+++ main.go
@@ -3,17 +3,21 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"live-voter-server/log"
 )
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Homepage, yo!")
+	log.Debug("Request: /")
 }
 
 func handleRequests() {
 	http.HandleFunc("/", homePage)
-	http.ListenAndServe(":8080", nil)
+	log.Error(http.ListenAndServe(":8080", nil).Error())
 }
 
 func main() {
+	log.Init("logs", "live-voter-server")
+	log.Info("Live Voter server START")
 	handleRequests()
 }
