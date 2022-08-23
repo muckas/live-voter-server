cc510d19530ddeeba1d799c039fa7d76b4a9e4fc
muckas <motobike8@outlook.com>
2022-08-23 13:13:16 +0500

    Receiving images

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	"net/http"
 	"encoding/json"
+	"io"
 	"io/ioutil"
 	"path/filepath"
 	"live-voter-server/log"
@@ -31,7 +32,7 @@ func check(w http.ResponseWriter, r *http.Request) {
 }
 
 func serveImage(w http.ResponseWriter, r *http.Request) {
-	var image_path string = filepath.Join("data", "test.png")
+	var image_path string = filepath.Join("data", "image.png")
 	buf, err := ioutil.ReadFile(image_path)
 	if err != nil {
 		log.Error(err)
@@ -41,10 +42,24 @@ func serveImage(w http.ResponseWriter, r *http.Request) {
 	log.Debug(r.RemoteAddr, r.URL, "image/png")
 }
 
+func uploadFile(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseMultipartForm(5 * 1024 * 1024)
+	if err != nil {
+		log.Error(err)
+	}
+	form_file, _, err := r.FormFile("fileupload")
+	defer form_file.Close()
+	upload_file, err := os.OpenFile(filepath.Join("data", "image.png"), os.O_WRONLY|os.O_CREATE, 0666)
+	defer upload_file.Close()
+	io.Copy(upload_file, form_file)
+	log.Debug(r.RemoteAddr, r.URL, "data/image.png")
+}
+
 func handleRequests() {
 	http.HandleFunc("/", matchAll)
 	http.HandleFunc("/check", check)
 	http.HandleFunc("/image", serveImage)
+	http.HandleFunc("/upload", uploadFile)
 	log.Error(http.ListenAndServe(":8080", nil))
 }
 
